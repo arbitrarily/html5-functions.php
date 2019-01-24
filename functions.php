30f69b8b41805c2008e35c2417a1568fc52fe27b
Marko <mbajlovic@gmail.com>
2019-01-24 03:08:47 -0500

    trim some fat

--- functions.php
+++ functions.php
@@ -23,31 +23,13 @@ if ( function_exists( 'add_theme_support' ) ) {
     add_image_size( 'medium', 250, '', true ); // Medium Thumbnail.
     add_image_size( 'small', 120, '', true ); // Small Thumbnail.
     add_image_size( 'custom-size', 700, 200, true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
-
-    // Add Support for Custom Backgrounds - Uncomment below if you're going to use.
-    /*add_theme_support('custom-background', array(
-    'default-color' => 'FFF',
-    'default-image' => get_template_directory_uri() . '/img/bg.jpg'
-    ));*/
-
-    // Add Support for Custom Header - Uncomment below if you're going to use.
-    /*add_theme_support('custom-header', array(
-    'default-image'          => get_template_directory_uri() . '/img/headers/default.jpg',
-    'header-text'            => false,
-    'default-text-color'     => '000',
-    'width'                  => 1000,
-    'height'                 => 198,
-    'random-default'         => false,
-    'wp-head-callback'       => $wphead_cb,
-    'admin-head-callback'    => $adminhead_cb,
-    'admin-preview-callback' => $adminpreview_cb
-    ));*/
+    add_image_size( 'custom-full', 1200, '', true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Enables post and comment RSS feed links to head.
     add_theme_support( 'automatic-feed-links' );
 
     // Enable HTML5 support.
-    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+    add_theme_support( 'html5', array( 'search-form', 'gallery', 'caption' ) );
 
     // Localisation Support.
     load_theme_textdomain( 'html5blank', get_template_directory() . '/languages' );
@@ -165,8 +147,7 @@ function html5blank_styles() {
 function register_html5_menu() {
     register_nav_menus( array( // Using array to specify more menus if needed
         'header-menu'  => esc_html( 'Header Menu', 'html5blank' ), // Main Navigation
-        'sidebar-menu' => esc_html( 'Sidebar Menu', 'html5blank' ), // Sidebar Navigation
-        'extra-menu'   => esc_html( 'Extra Menu', 'html5blank' ) // Extra Navigation if needed (duplicate as many as you need!)
+        'footer-menu' => esc_html( 'Footer Menu', 'html5blank' ), // Sidebar Navigation
     ) );
 }
 
@@ -209,32 +190,6 @@ function remove_width_attribute( $html ) {
     return $html;
 }
 
-
-// If Dynamic Sidebar Exists
-if ( function_exists( 'register_sidebar' ) ) {
-    // Define Sidebar Widget Area 1
-    register_sidebar( array(
-        'name'          => esc_html( 'Widget Area 1', 'html5blank' ),
-        'description'   => esc_html( 'Description for this widget-area...', 'html5blank' ),
-        'id'            => 'widget-area-1',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>',
-    ) );
-
-    // Define Sidebar Widget Area 2
-    register_sidebar( array(
-        'name'          => esc_html( 'Widget Area 2', 'html5blank' ),
-        'description'   => esc_html( 'Description for this widget-area...', 'html5blank' ),
-        'id'            => 'widget-area-2',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>',
-    ) );
-}
-
 // Remove wp_head() injected Recent Comment styles
 function my_remove_recent_comments_style() {
     global $wp_widget_factory;
@@ -257,38 +212,6 @@ function html5wp_pagination() {
         'current' => max( 1, get_query_var( 'paged' ) ),
         'total'   => $wp_query->max_num_pages,
     ) );
-}
-
-// Create 20 Word Callback for Index page Excerpts, call using html5wp_excerpt('html5wp_index');
-function html5wp_index( $length ) {
-    return 20;
-}
-
-// Create 40 Word Callback for Custom Post Excerpts, call using html5wp_excerpt('html5wp_custom_post');
-function html5wp_custom_post( $length ) {
-    return 40;
-}
-
-// Create the Custom Excerpts callback
-function html5wp_excerpt( $length_callback = '', $more_callback = '' ) {
-    global $post;
-    if ( function_exists( $length_callback ) ) {
-        add_filter( 'excerpt_length', $length_callback );
-    }
-    if ( function_exists( $more_callback ) ) {
-        add_filter( 'excerpt_more', $more_callback );
-    }
-    $output = get_the_excerpt();
-    $output = apply_filters( 'wptexturize', $output );
-    $output = apply_filters( 'convert_chars', $output );
-    $output = '<p>' . $output . '</p>';
-    echo esc_html( $output );
-}
-
-// Custom View Article link to Post
-function html5_blank_view_article( $more ) {
-    global $post;
-    return '... <a class="view-article" href="' . get_permalink( $post->ID ) . '">' . esc_html( 'View Article', 'html5blank' ) . '</a>';
 }
 
 // Remove Admin bar
@@ -314,58 +237,6 @@ function html5blankgravatar ( $avatar_defaults ) {
     return $avatar_defaults;
 }
 
-// Threaded Comments
-function enable_threaded_comments() {
-    if ( ! is_admin() ) {
-        if ( is_singular() AND comments_open() AND ( get_option( 'thread_comments' ) == 1 ) ) {
-            wp_enqueue_script( 'comment-reply' );
-        }
-    }
-}
-
-// Custom Comments Callback
-function html5blankcomments( $comment, $args, $depth ) {
-    $GLOBALS['comment'] = $comment;
-    extract( $args, EXTR_SKIP );
-
-    if ( 'div' == $args['style'] ) {
-        $tag       = 'div';
-        $add_below = 'comment';
-    } else {
-        $tag       = 'li';
-        $add_below = 'div-comment';
-    }
-?>
-    <!-- heads up: starting < for the html tag (li or div) in the next line: -->
-    <<?php echo esc_html( $tag ) ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID(); ?>">
-    <?php if ( 'div' != $args['style'] ) : ?>
-    <div id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-    <?php endif; ?>
-    <div class="comment-author vcard">
-    <?php if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-    <?php printf( esc_html( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ) ?>
-    </div>
-<?php if ( $comment->comment_approved == '0' ) : ?>
-    <em class="comment-awaiting-moderation"><?php esc_html( 'Your comment is awaiting moderation.' ) ?></em>
-    <br />
-<?php endif; ?>
-
-    <div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-        <?php
-            printf( esc_html( '%1$s at %2$s' ), get_comment_date(), get_comment_time() ) ?></a><?php edit_comment_link( esc_html( '(Edit)' ), '  ', '' );
-        ?>
-    </div>
-
-    <?php comment_text() ?>
-
-    <div class="reply">
-    <?php comment_reply_link( array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ) ?>
-    </div>
-    <?php if ( 'div' != $args['style'] ) : ?>
-    </div>
-    <?php endif; ?>
-<?php }
-
 /*------------------------------------*\
     Actions + Filters + ShortCodes
 \*------------------------------------*/
@@ -373,7 +244,6 @@ function html5blankcomments( $comment, $args, $depth ) {
 // Add Actions
 add_action( 'wp_enqueue_scripts', 'html5blank_header_scripts' ); // Add Custom Scripts to wp_head
 add_action( 'wp_print_scripts', 'html5blank_conditional_scripts' ); // Add Conditional Page Scripts
-add_action( 'get_header', 'enable_threaded_comments' ); // Enable Threaded Comments
 add_action( 'wp_enqueue_scripts', 'html5blank_styles' ); // Add Theme Stylesheet
 add_action( 'init', 'register_html5_menu' ); // Add HTML5 Blank Menu
 add_action( 'widgets_init', 'my_remove_recent_comments_style' ); // Remove inline Recent Comment Styles from wp_head()
